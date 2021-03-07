6c213eaa8a8b7424da614dd3e10b53d155d3c1a7
Jesper Stroebaek <hamdererindebatman@protonmail.com>
2021-03-07 13:53:44 +0100

    1

--- src/main/java/com/example/demo/controllers/FormExample.java
+++ src/main/java/com/example/demo/controllers/FormExample.java
@@ -2,25 +2,28 @@ package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 
 @Controller
 public class FormExample {
+
     // Showing how to create a form using thymeleaf
     @GetMapping(value = "/")
     public String renderForm() {
         return "form";
     }
 
-    @GetMapping(value="/list")
+    @PostMapping("/list")
+    //@GetMapping(value="/list")
     @ResponseBody
     public ArrayList list() {
         ArrayList<String> mylist = new ArrayList<>();
         mylist.add("peel my");
         mylist.add("orange");
-
         return mylist;
     }
 }
